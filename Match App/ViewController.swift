c0714fec47f03ac0c6dffcdd6db6968a44bbc0bd
Allen Farcas <allen.farcas@student.upt.ro>
2018-12-03 13:55:11 +0200

    Card flipping done

--- Match App/ViewController.swift
+++ Match App/ViewController.swift
@@ -15,12 +15,49 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        // Get a CarcCollectionView Cell object
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
+        
+        // Get the card that the collection view is trying to display
+        let card = cardArray[indexPath.row]
+        
+        // Set that card for the cell
+        cell.setCard(card)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // Get the cell that the user selected
+        let cell = collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
         
+        // Get the card that the user selected
+        let card = cardArray[indexPath.row]
+        
+        if card.isFlipped == false {
+            
+            // Flip the card
+            cell.flip()
+            
+            // Set the status of the card
+            card.isFlipped = true
+            
+            // Determine if it's the first card or second card that's flipped over
+            if firstFlippedCard == nil {
+                // This is the first card being flipped
+                firstFlippedCard = indexPath
+            } else {
+                // This is the second card being flipped
+                
+                // TODO: Perform the matching logic
+            }
+        } else  {
+            
+            // Flip the card back
+            cell.flipBack()
+            
+            // Set the status of the card
+            card.isFlipped = false
+        }
     }
     
 
@@ -28,6 +65,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     var model = CardModel()
     var cardArray = [Card]() // or var cardArray:[Card]?
+    
+    var firstFlippedCard:IndexPath?
     
     override func viewDidLoad() {
         super.viewDidLoad()
