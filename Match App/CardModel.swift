ed004e76f1b17bfe003630e383b285b6973e8b88
Allen Farcas <allen.farcas@student.upt.ro>
2018-12-03 15:09:00 +0200

    Game logic done

--- Match App/CardModel.swift
+++ Match App/CardModel.swift
@@ -12,25 +12,32 @@ class CardModel {
     func getCard () -> [Card] {
         // Declare an array to store the generated cards
         var generatedCardArray = [Card]()
+        var randomArray = [Int]()
         
         // Randomly generate pairs of cards
-        for _ in 1...8 {
+        repeat {
             // Get a random number
-            let randomNumber = arc4random_uniform(13) + 1
-            // Log the number
-            print("Generating a random number: \(randomNumber)")
+            let randomNumber:Int = Int(arc4random_uniform(13) + 1)
+            
             // Create the first card object
             let cardOne = Card()
             cardOne.imageName = "card\(randomNumber)"
-            generatedCardArray.append(cardOne)
             
-            // Create the second card object
-            let cardTwo = Card()
-            cardTwo.imageName = "card\(randomNumber)"
-            generatedCardArray.append(cardTwo)
-            
+            // Check if the number exists already
+            if randomArray.contains(randomNumber) == false {
+                
+                // Log the number
+                print("Generating a random number: \(randomNumber)")
+                randomArray.append(randomNumber)
+                generatedCardArray.append(cardOne)
+
+                // Create the second card object
+                let cardTwo = Card()
+                cardTwo.imageName = "card\(randomNumber)"
+                generatedCardArray.append(cardTwo)
+            }
             // OPTIONAL: Make it so we only have unique pairs of cards
-        }
+        } while generatedCardArray.count < 16
         
         // TODO: Randomize the array
         
--- Match App/ViewController.swift
+++ Match App/ViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var model = CardModel()
+    var cardArray = [Card]() // or var cardArray:[Card]?
+    
+    var firstFlippedCardIndex:IndexPath?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        // Call the getCards method from the card model
+        cardArray = model.getCard()
+    }
+    
     // MARK: - UICollectionViewProtocols Methods
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return cardArray.count;
@@ -33,7 +51,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         // Get the card that the user selected
         let card = cardArray[indexPath.row]
         
-        if card.isFlipped == false {
+        if card.isFlipped == false  && card.isMatched == false {
             
             // Flip the card
             cell.flip()
@@ -42,40 +60,61 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             card.isFlipped = true
             
             // Determine if it's the first card or second card that's flipped over
-            if firstFlippedCard == nil {
+            if firstFlippedCardIndex == nil {
+                
                 // This is the first card being flipped
-                firstFlippedCard = indexPath
+                firstFlippedCardIndex = indexPath
+                
             } else {
                 // This is the second card being flipped
                 
                 // TODO: Perform the matching logic
+                checkForMatches(indexPath)
             }
-        } else  {
-            
-            // Flip the card back
-            cell.flipBack()
-            
-            // Set the status of the card
-            card.isFlipped = false
         }
+    } // End the didSelectItemAt method
+    
+    // MARK: - Game Logic Methods
+    func checkForMatches(_ secondFlippedCardIndex:IndexPath){
+        // Get the cells for the two cards that were revealed
+        let cardOneCell = collectionView.cellForItem(at: firstFlippedCardIndex!) as? CardCollectionViewCell
+        let cardTwoCell = collectionView.cellForItem(at: secondFlippedCardIndex) as? CardCollectionViewCell
+        
+        //Get the card for the two cards that were revealed
+        let cardOne = cardArray[firstFlippedCardIndex!.row]
+        let cardTwo = cardArray[secondFlippedCardIndex.row]
+        
+        // Compare the two cards
+        if cardOne.imageName == cardTwo.imageName {
+            // It's a match
+            
+            // Set the statuses of the cards
+            cardOne.isMatched = true
+            cardTwo.isMatched = true
+            
+            // Remove the card from the grid
+            cardOneCell?.remove()
+            cardTwoCell?.remove()
+        } else {
+            // It's not a match
+            
+            // Set the statuses of the cards
+            cardOne.isFlipped = false
+            cardTwo.isFlipped = false
+            
+            // Flip both cards back
+            cardOneCell?.flipBack()
+            cardTwoCell?.flipBack()
+        }
+        
+        // Tell the collectionView to reload the cell of the first card if it is nil
+        if cardOneCell == nil {
+            collectionView.reloadItems(at: [firstFlippedCardIndex!])
+        }
+        
+        //Reset the property that tracks the first card flipped
+        firstFlippedCardIndex = nil
     }
     
-
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    var model = CardModel()
-    var cardArray = [Card]() // or var cardArray:[Card]?
-    
-    var firstFlippedCard:IndexPath?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        // Call the getCards method from the card model
-        cardArray = model.getCard()
-    }
-
-
-}
+} // End the ViewController class
 
